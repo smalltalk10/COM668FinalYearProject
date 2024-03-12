d17fc309b2b36ebaa589f3a23b7987240e7224f7
smalltalk10 <niall.morrissey@hotmail.com>
2024-03-12 14:00:46 +0000

    Add login functionality to account registration modal

--- src/app/nav/loginModal/authentication.modal.component.ts
+++ src/app/nav/loginModal/authentication.modal.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, Validators } from '@angular/forms';
 import { WebService } from '../../web.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-modal',
@@ -13,13 +14,19 @@ export class AuthenticationModalComponent {
     public webService: WebService,
     public activeModal: NgbActiveModal,
     private formBuilder: FormBuilder,
+    private router: Router,
   ) {}
 
+  loginForm: any;
   registerForm: any;
   errorMessage: string = '';
   emailIsValid: boolean = false;
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
     this.registerForm = this.formBuilder.group({
       username: ['', Validators.required],
       email: [
@@ -37,6 +44,28 @@ export class AuthenticationModalComponent {
 
   closeModal() {
     this.activeModal.dismiss();
+  }
+
+  loginIsInvalid(control: any) {
+    return (
+      this.loginForm.controls[control].invalid &&
+      this.loginForm.controls[control].touched
+    );
+  }
+  
+  loginIsUntouched() {
+    return (
+      this.loginForm.controls.username.pristine ||
+      this.loginForm.controls.password.pristine
+    );
+  }
+
+  loginIsIncomplete() {
+    return (
+      this.loginIsInvalid('username') ||
+      this.loginIsInvalid('password') ||
+      this.loginIsUntouched()
+    );
   }
 
   updateEmailValidity() {
@@ -68,6 +97,21 @@ export class AuthenticationModalComponent {
       this.registerIsUntouched() ||
       !this.emailIsValid
     );
+  }
+
+  onSubmitUserLogin() {
+    this.webService.login(this.loginForm.value).subscribe({
+      next: (response: any) => {
+          sessionStorage.setItem('token', response.token);
+          this.closeModal();
+          this.router.navigateByUrl('/dashboard');
+      },
+      error: (error) => {
+        console.error('HTTP error:', error);
+        const decodedMessage = error?.error?.message || 'Unknown error';
+        this.errorMessage = 'Login failed: ' + decodedMessage;
+      },
+    });
   }
 
   onSubmitUserRegister() {
--- src/app/web.service.ts
+++ src/app/web.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -15,10 +15,30 @@ export class WebService {
     'rest/v1/users?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=hK7QlQm25PYY7y46HmVHj35p4HUwMdS-Tk0si0YZH0A', postData);
   }
 
+  login(loginDetails: any) {
+    const headers = new HttpHeaders()
+      .set('username', loginDetails.username)
+      .set('password', loginDetails.password)
+      .set('Content-Type', 'application/json');
+
+    return this.http.get('https://prod-13.uksouth.logic.azure.com/workflows/0ea96f9b552947f5aae1f399ba91c283/triggers/manual/paths/invoke/'+
+    'rest/v1/users/session?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=oFKwoT8a-IGsW62ZuAVLNH3gx7DxrCd2Gpnj6t-arEg', 
+    { headers });
+  }
+
+  // Authenticated Requests
+  getHeaders() {
+    const token: string = sessionStorage.getItem('token') || '';
+    return new HttpHeaders({
+      'x-access-token': token,
+    });
+  }
+
   getCurrentMeasurements() {
+    const headers = this.getHeaders();
     return this.http.get(
       'https://prod-05.uksouth.logic.azure.com/workflows/d2d30c052eec437f992963398acae520/triggers/manual/paths/invoke/'+
-      'rest/v1/measurement?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=2dhbufTAe3z1ejRAPIJ14nOcON-KB_h2MVEqFATn_0U'
-      )
+      'rest/v1/measurement?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=2dhbufTAe3z1ejRAPIJ14nOcON-KB_h2MVEqFATn_0U',
+      { headers });
     }
 }
